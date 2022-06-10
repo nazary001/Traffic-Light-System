7aa0ab55f8b8ef80f49a70095b6e6221e526c9e2
nazary001 <nazary9911@gmail.com>
2022-06-10 23:53:34 +0300

    add some changes

--- app.js
+++ app.js
@@ -1,133 +1,157 @@
-let sec1_1 = document.getElementById('sec1_1');
-let sec1_2 = document.getElementById('sec1_2');
-let sec1_3 = document.getElementById('sec1_3');
-let sec2_1 = document.getElementById('sec2_1');
-let sec2_2 = document.getElementById('sec2_2');
-let sec2_3 = document.getElementById('sec2_3');
-let sec3_1 = document.getElementById('sec3_1');
-let sec3_2 = document.getElementById('sec3_2');
-let sec3_3 = document.getElementById('sec3_3');
-let sec4_1 = document.getElementById('sec4_1');
-let sec4_2 = document.getElementById('sec4_2');
-let sec4_3 = document.getElementById('sec4_3');
-let start = document.getElementById('start_button');
-let stop = document.getElementById('stop_button');
+const t1_red = document.querySelector('#t1 .red');
+const t1_amber = document.querySelector('#t1 .amber');
+const t1_green = document.querySelector('#t1 .green');
+
+const t2_red = document.querySelector('#t2 .red');
+const t2_amber = document.querySelector('#t2 .amber');
+const t2_green = document.querySelector('#t2 .green');
+
+const t3_red = document.querySelector('#t3 .red');
+const t3_amber = document.querySelector('#t3 .amber');
+const t3_green = document.querySelector('#t3 .green');
 
 
-const traffic_light_1 = [sec1_1, sec1_2, sec1_3];
-const traffic_light_2 = [sec2_1, sec2_2, sec2_3];
-const traffic_light_3 = [sec3_1, sec3_2, sec3_3];
-const traffic_light_4 = [sec4_1, sec4_2, sec4_3];
+const t4_red = document.querySelector('#t4 .red');
+const t4_amber = document.querySelector('#t4 .amber');
+const t4_green = document.querySelector('#t4 .green');
 
-sec1_2.style.backgroundColor = 'gray';
-sec2_2.style.backgroundColor = 'gray';
-sec3_2.style.backgroundColor = 'gray';
+const start = document.getElementById('start_button');
+const stop = document.getElementById('stop_button');
 
-function standby_mode(){
-    if(traffic_light_1[1].style.backgroundColor == 'gray'){
-        traffic_light_1[1].style.backgroundColor = 'yellow';
-        traffic_light_2[1].style.backgroundColor = 'yellow';
-        traffic_light_3[1].style.backgroundColor = 'yellow';
-        traffic_light_4[1].style.backgroundColor = 'yellow';
+
+class TrafficLight {
+    constructor(red, amber, green) {
+        this.red = red;
+        this.amber = amber;
+        this.green = green;
+        this.standById;
+        this.green_blink;
+        this.init();
     }
-    else if(traffic_light_1[1].style.backgroundColor == 'yellow'){
-        traffic_light_1[1].style.backgroundColor = 'gray';
-        traffic_light_2[1].style.backgroundColor = 'gray';
-        traffic_light_3[1].style.backgroundColor = 'gray';
-        traffic_light_4[1].style.backgroundColor = 'gray';
+
+    init() {
+        this.red.style.backgroundColor = 'gray';
+        this.amber.style.backgroundColor = 'gray';
+        this.green.style.backgroundColor = 'gray';
     }
+
+    standby() {
+        let state = 1;
+         this.standById = setInterval((t) => {
+            if(state === 1) {
+                t.setColor({red: 0, amber: 0, green: 0});
+                state = 0;
+            } else {
+                t.setColor({red: 0, amber: 1, green: 0});
+                state = 1;
+            }
+        }, 1000, this);
+    }
+
+    
+    setColor({red, amber, green}) {
+        this.red.style.backgroundColor = (red === 1) ?  "red" : "gray";
+        this.amber.style.backgroundColor = (amber === 1) ? "yellow" : "gray";
+        this.green.style.backgroundColor = (green === 1) ? "green" : "gray";
+    }
+
+    stopStandBy(){
+        clearInterval(this.standById);
+        this.init();
+    }
+
+    working(){
+        this.setColor({red: 1, amber: 0, green: 0});
+        setTimeout(() => {this.setColor({red: 1, amber: 1, green: 0})}, 8000);
+        setTimeout(() => {this.setColor({red: 0, amber: 0, green: 1})}, 10000);
+        setTimeout(() => {
+            let state = 1;
+             this.green_blink = setInterval(() =>{
+                if(state === 1){
+                    this.setColor({red: 0, amber: 0, green: 0 });
+                    state = 0;
+                }
+                else{
+                    this.setColor({red: 0, amber: 0, green: 1 });
+                    state = 1;
+                }
+            }, 500)
+        },14000)
+        setTimeout(() => {
+            clearInterval(this.green_blink);
+            this.setColor({red: 0, amber: 1, green: 0 });
+        }, 18000)
+        setTimeout(() => {this.setColor({red: 0, amber: 0, green: 0 })}, 20000);
+    }
+
+    working2() {
+        this.setColor({red: 0, amber: 0, green: 1});
+        setTimeout(() => {
+            let state = 1;
+             this.green_blink = setInterval(() =>{
+                if(state === 1){
+                    this.setColor({red: 0, amber: 0, green: 0 });
+                    state = 0;
+                }
+                else{
+                    this.setColor({red: 0, amber: 0, green: 1 });
+                    state = 1;
+                }
+            }, 500)
+        },4000)
+        setTimeout(() => {
+            clearInterval(this.green_blink);
+            this.setColor({red: 0, amber: 1, green: 0})}, 8000);
+        setTimeout(() => {this.setColor({red: 1, amber: 0, green: 0})}, 10000);
+        setTimeout(() => {this.setColor({red: 0, amber: 1, green: 0})}, 18000);
+        setTimeout(() => {this.setColor({red: 0, amber: 0, green: 0 })}, 20000);
+    }
+
 }
 
- let standby_stop = setInterval(standby_mode, 1000);
+const t1 = new TrafficLight(t1_red, t1_amber, t1_green);
+const t2 = new TrafficLight(t2_red, t2_amber, t2_green);
+const t3 = new TrafficLight(t3_red, t3_amber, t3_green);
+const t4 = new TrafficLight(t4_red, t4_amber, t4_green);
+
+t1.standby();
+t2.standby();
+t3.standby();
+t4.standby();
+
 
 start.addEventListener('click', () => {
     start.style.display = 'none';
-    clearInterval(standby_stop);
-    if(traffic_light_1[1].style.backgroundColor == 'yellow'){
-        standby_mode();
+    t1.stopStandBy();
+    t2.stopStandBy();
+    t3.stopStandBy();
+    t4.stopStandBy();
+
+    t1.working();
+    t2.working2();
+    t3.working2();
+    t4.working();
+
+    let work = setInterval(() => {
+    t1.working();
+    t2.working2();
+    t3.working2();
+    t4.working();
+    }, 20000);
+});
+
+stop.addEventListener('click', () => {
+    start.style.display = '';
+    let highestTimeoutId = setTimeout(";");
+    for (var i = 0 ; i < highestTimeoutId ; i++) {
+        clearTimeout(i); 
     };
-    function working(){
-        traffic_light_1[2].style.backgroundColor = 'red';
-        traffic_light_4[0].style.backgroundColor = 'red';
-        traffic_light_2[0].style.backgroundColor = 'green';
-        traffic_light_3[2].style.backgroundColor = 'green';
-
-        setTimeout(() => {
-            traffic_light_1[2].style.backgroundColor = 'gray';
-            traffic_light_1[1].style.backgroundColor = 'gray';
-            traffic_light_1[0].style.backgroundColor = 'green';
-            traffic_light_4[0].style.backgroundColor = 'gray';
-            traffic_light_4[1].style.backgroundColor = 'gray';
-            traffic_light_4[2].style.backgroundColor = 'green';
-            traffic_light_3[1].style.backgroundColor = 'gray';
-            traffic_light_2[1].style.backgroundColor = 'gray';
-            traffic_light_3[0].style.backgroundColor = 'red';
-            traffic_light_2[2].style.backgroundColor = 'red';
-
-        }, 10000);
-        setTimeout(() => {end_green_2 = setInterval(green_light_2, 500)}, 4000)
-        setTimeout(() => {
-            traffic_light_1[1].style.backgroundColor = 'yellow';
-            traffic_light_4[1].style.backgroundColor = 'yellow';
-            traffic_light_2[1].style.backgroundColor = 'yellow';
-            traffic_light_3[1].style.backgroundColor = 'yellow';
-            clearInterval(end_green_2);
-         }, 8000);
-        function green_light() {
-            if(traffic_light_1[0].style.backgroundColor == 'green'){
-                traffic_light_1[0].style.backgroundColor = 'gray';
-                traffic_light_4[2].style.backgroundColor = 'gray';
-            }
-            else if(traffic_light_1[0].style.backgroundColor == 'gray'){
-                traffic_light_1[0].style.backgroundColor = 'green';
-                traffic_light_4[2].style.backgroundColor = 'green';
-            }
-        };
-        function green_light_2() {
-            if(traffic_light_2[0].style.backgroundColor == 'green'){
-                traffic_light_2[0].style.backgroundColor = 'gray';
-                traffic_light_3[2].style.backgroundColor = 'gray';
-            }
-            else if(traffic_light_2[0].style.backgroundColor == 'gray'){
-                traffic_light_2[0].style.backgroundColor = 'green';
-                traffic_light_3[2].style.backgroundColor = 'green';
-            }
-        };
-        setTimeout(() => {end_green = setInterval(green_light, 500)}, 16000);
-        setTimeout(() => {
-            clearInterval(end_green);
-            traffic_light_1[1].style.backgroundColor = 'yellow';
-            traffic_light_4[1].style.backgroundColor = 'yellow';
-            traffic_light_2[1].style.backgroundColor = 'yellow';
-            traffic_light_3[1].style.backgroundColor = 'yellow';
-        }, 20000);
-        setTimeout(() => {
-            traffic_light_1[1].style.backgroundColor = 'gray';
-            traffic_light_4[1].style.backgroundColor = 'gray';
-            traffic_light_2[1].style.backgroundColor = 'gray';
-            traffic_light_3[1].style.backgroundColor = 'gray';
-            traffic_light_2[2].style.backgroundColor = 'gray';
-            traffic_light_3[0].style.backgroundColor = 'gray';
-        }, 22000);
-        
-    };
-    working();
-    stopInt = setInterval(working, 22000);
-    stop.addEventListener('click', () => {
-        start.style.display = '';
-        clearInterval(stopInt);
-        let kill_time = setTimeout(';');
-        for(i = 0; i < kill_time; i++){
-            clearTimeout(i);
-        }
-
-        for(let i = 0; i <= 2; i++){
-            traffic_light_1[i].style.backgroundColor = 'gray';
-            traffic_light_2[i].style.backgroundColor = 'gray';
-            traffic_light_3[i].style.backgroundColor = 'gray';
-            traffic_light_4[i].style.backgroundColor = 'gray';
-        }
-
-        standby_stop = setInterval(standby_mode, 1000);
-    })
+    t1.init();
+    t2.init();
+    t3.init();
+    t4.init();
+    t1.standby();
+    t2.standby();
+    t3.standby();
+    t4.standby();
 });
